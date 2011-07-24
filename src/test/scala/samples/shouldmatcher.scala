aa88de14272fbda6b94c03e165c413b7fd5502f7
Alex Bleasdale <ableasdale@marklogic.com>
2011-07-24 11:56:08 +0100

    adding more should examples to ensure CI integration is working properly

--- src/test/scala/samples/shouldmatcher.scala
+++ src/test/scala/samples/shouldmatcher.scala
@@ -11,5 +11,12 @@ class SampleTest extends Spec with ShouldMatchers {
     it("should run") {
       1 + 1 should be(2)
     }
+    it("should be able to add") {
+      2 + 2 should be(4)
+    }
+    it("should be able to multiply") {
+      8 * 8 should be(64)
+    }
+
   }
 }
